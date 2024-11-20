9b6bf73753a231d52fd4bf488cc990a4f0a6e02d
CG <1499046294@qq.com>
2024-11-20 10:57:20 +0800

    Second Modify

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*******************************************************************************
+*Create By CG
+*Function 
+*******************************************************************************/
+namespace GameManger
+{
+	public class GameManager : MonoBehaviour
+	{
+		    
+	}
+}
