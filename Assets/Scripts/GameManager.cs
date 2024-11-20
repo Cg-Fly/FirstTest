7f9de8d51f3ec3b8edefa16af0426d36442b4838
CG <1499046294@qq.com>
2024-11-20 17:37:19 +0800

    改仓库主分支

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ namespace CG
             Debug.Log("在function分支进行修改");
             Debug.Log("在function分支进行第二次修改");
             Debug.Log("主分支创建并修改");
+            Debug.Log("远程主分支改支创建并修改");
         }
     }
 }
